dd41aea87c5367f17b3d5ea47c68447f54c8f03e
Samael509 <pavel718234@gmail.com>
2026-02-18 14:05:28 +0300

    lists in kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -14,6 +14,26 @@ val newSolarSystem = arrayOf(
     "pluto"
 )
 
-fun main(){
-    println(newSolarSystem[8])
+fun main() {
+    val solarSystem = mutableListOf(
+        "mercury", "venus", "earth", "mars", "jupiter",
+        "saturn", "uranus", "neptune"
+    )
+    println(newSolarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("earth"))
+    println(solarSystem.indexOf("pluto"))
+
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem.add("pluto")
+    solarSystem.add(3, "pluto")
+    solarSystem[3] = "future moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    println(solarSystem.contains("pluto"))
+    println("future moon" in solarSystem)
 }
