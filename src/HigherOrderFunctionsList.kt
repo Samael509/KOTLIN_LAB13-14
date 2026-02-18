48122b46ab0932680be51b2d309d29c1e12e8dac
Samael509 <pavel718234@gmail.com>
2026-02-18 14:51:15 +0300

    forEach() function

---
+++ src/HigherOrderFunctionsList.kt
@@ -0,0 +1,53 @@
+import java.text.CollationKey
+
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double,
+)
+
+val cookie = listOf(
+    Cookie(
+        name = "печенье с шоколадом",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),Cookie(
+        name = "нуга ореховая",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),Cookie(
+        name = "ванильный крем",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),Cookie(
+        name = "шоколадно-арахисовое масло",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),Cookie(
+        name = "Сникерс",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),Cookie(
+        name = "черничный пирог",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),Cookie(
+        name = "сахар и посыпка",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    ),
+)
+
+fun main(){
+    cookie.forEach(){
+        println("пункт меню: ${it.name}")
+    }
+}
