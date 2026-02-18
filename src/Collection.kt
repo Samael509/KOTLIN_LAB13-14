fb12c86710ee543a6f0b26ee2c3a0697c53704a8
Samael509 <pavel718234@gmail.com>
2026-02-18 14:17:16 +0300

    sets in kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -19,21 +19,30 @@ fun main() {
         "mercury", "venus", "earth", "mars", "jupiter",
         "saturn", "uranus", "neptune"
     )
-    println(newSolarSystem.size)
-    println(solarSystem[2])
-    println(solarSystem.get(3))
-    println(solarSystem.indexOf("earth"))
-    println(solarSystem.indexOf("pluto"))
-
-    for (planet in solarSystem) {
-        println(planet)
-    }
     solarSystem.add("pluto")
-    solarSystem.add(3, "pluto")
-    solarSystem[3] = "future moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
-    solarSystem.removeAt(9)
+    println(solarSystem.size)
     println(solarSystem.contains("pluto"))
-    println("future moon" in solarSystem)
+    println("pluto" in solarSystem)
+    solarSystem.add("pluto")
+    println(solarSystem.size)
+    solarSystem.remove("pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("pluto"))
+//    println(newSolarSystem.size)
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//    println(solarSystem.indexOf("earth"))
+//    println(solarSystem.indexOf("pluto"))
+//
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+//    solarSystem.add("pluto")
+//    solarSystem.add(3, "pluto")
+//    solarSystem[3] = "future moon"
+//    println(solarSystem[3])
+//    println(solarSystem[9])
+//    solarSystem.removeAt(9)
+//    println(solarSystem.contains("pluto"))
+//    println("future moon" in solarSystem)
 }
